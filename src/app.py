1f8cfde778aea19e3a21026b77c0030cedea257c
Alejandro <64385399+Fractal-Vortex@users.noreply.github.com>
2024-12-11 13:31:20 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -1,19 +1,94 @@
 """
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+This module takes care of starting the API Server, Loading the DB, and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+# create the Jackson family object
 jackson_family = FamilyStructure("Jackson")
+
+# -------------------------------------------------------------
+
+@app.route('/members', methods=['GET'])
+def get_members():
+    return jsonify(jackson_family.get_all_members()), 200
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.get_json()
+    jackson_family.add_member(member)
+    return jsonify({"msg": "Miembro anadido correctamente", "member": member})
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if not member:
+        return jsonify({"error": "Miembro no encontrado"}), 404
+    jackson_family.delete_member(member_id)
+    return jsonify({"msg": "Miembro eliminado correctamente", "Miembro Eliminado": True}), 200
+
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if not member:
+        return jsonify({"msg": "El miembro no existe"}), 400
+    jackson_family.get_member(member_id)
+    return jsonify(member, {"msg": "Miembro localizado correctamente", "Miembro  localizado": True}), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -------------------------------------------------------------
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -24,19 +99,6 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
-@app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
